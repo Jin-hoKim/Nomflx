4a445d5a73e8629d51b7af70845bb229e5d8e8a1
JHKim <wltn222@gmail.com>
2021-06-01 11:48:37 +0900

    1

--- src/index.js
+++ src/index.js
@@ -1,11 +1,5 @@
-import { StrictMode } from "react";
 import ReactDOM from "react-dom";
 import App from "./Components/App";
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-  rootElement
-);
+ReactDOM.render(<App />, rootElement);
