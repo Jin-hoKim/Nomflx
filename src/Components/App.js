24652c5c84a9154d3df12ed48524a9041d387def
JHKim <wltn222@gmail.com>
2021-06-01 11:44:12 +0900

    Initial commit

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Router } from "react-router-dom";
+
+class App extends Component {
+  render() {
+    return (
+      <>
+        <Router />
+      </>
+    );
+  }
+}
+
+export default App;
